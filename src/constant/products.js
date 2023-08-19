5d8eb8d2eeeb40e2ce349d3b1c19e07d42dcd3d1
PriyanshuSharma0326 <ps940016@gmail.com>
2023-08-19 23:15:54 +0530

    Add product images

--- src/constant/products.js
+++ src/constant/products.js
@@ -2,161 +2,161 @@ const products = [
     {
         id: 1,
         productName: 'iPhone 13 Pro',
-        productImageURL: '/assets/item_1.png',
+        productImageURL: '/assets/iphone13pro.png',
         productCategory: 'Mobile Phones',
-        productPrice: '$1099',
+        productPrice: '1099',
         productRating: ''
     },
     {
         id: 2,
         productName: 'Samsung Galaxy S21',
-        productImageURL: '',
+        productImageURL: '/assets/galaxys21.png',
         productCategory: 'Mobile Phones',
-        productPrice: '$899',
+        productPrice: '899',
         productRating: ''
     },
     {
         id: 3,
         productName: 'Leather Jacket',
-        productImageURL: '',
+        productImageURL: '/assets/leather_jacket.png',
         productCategory: 'Jackets',
-        productPrice: '$149',
+        productPrice: '149',
         productRating: ''
     },
     {
         id: 4,
         productName: 'Faux Fur Coat',
-        productImageURL: '',
+        productImageURL: '/assets/faux_fur_coat.png',
         productCategory: 'Jackets',
-        productPrice: '$99',
+        productPrice: '99',
         productRating: ''
     },
     {
         id: 5,
         productName: 'Leather Wallet',
-        productImageURL: '',
+        productImageURL: '/assets/leather_wallet.png',
         productCategory: 'Wallets',
-        productPrice: '$29',
+        productPrice: '29',
         productRating: ''
     },
     {
         id: 6,
         productName: 'Smartwatch',
-        productImageURL: '',
+        productImageURL: '/assets/smartwatch.png',
         productCategory: 'Watches',
-        productPrice: '$199',
+        productPrice: '199',
         productRating: ''
     },
     {
         id: 7,
-        productName: 'Sony Noise-Cancelling Headphones',
-        productImageURL: '',
+        productName: 'Sony Headphones',
+        productImageURL: '/assets/sony_headphones.png',
         productCategory: 'Electronics',
-        productPrice: '$249',
+        productPrice: '249',
         productRating: ''
     },
     {
         id: 8,
         productName: 'Dell Inspiron Laptop',
-        productImageURL: '',
+        productImageURL: '/assets/dell_inspiron.png',
         productCategory: 'Electronics',
-        productPrice: '$899',
+        productPrice: '899',
         productRating: ''
     },
     {
         id: 9,
-        productName: 'Nike Air Max Sneakers',
-        productImageURL: '',
+        productName: 'Nike Air Max',
+        productImageURL: '/assets/nike_air_max.png',
         productCategory: 'Shoes',
-        productPrice: '$129',
+        productPrice: '129',
         productRating: ''
     },
     {
         id: 10,
-        productName: 'Adidas Ultraboost Running Shoes',
-        productImageURL: '',
+        productName: 'Adidas Ultraboost',
+        productImageURL: '/assets/adidas_shoes.png',
         productCategory: 'Shoes',
-        productPrice: '$149',
+        productPrice: '149',
         productRating: ''
     },
     {
         id: 11,
         productName: 'Cotton T-Shirt',
-        productImageURL: '',
+        productImageURL: '/assets/cotton_tshirt.png',
         productCategory: 'Clothing',
-        productPrice: '$19',
+        productPrice: '19',
         productRating: ''
     },
     {
         id: 12,
         productName: 'Denim Jeans',
-        productImageURL: '',
+        productImageURL: '/assets/denim_jeans.png',
         productCategory: 'Clothing',
-        productPrice: '$39',
+        productPrice: '39',
         productRating: ''
     },
     {
         id: 13,
         productName: 'Swiss Army Knife',
-        productImageURL: '',
+        productImageURL: '/assets/swiss_knife.png',
         productCategory: 'Tools',
-        productPrice: '$49',
+        productPrice: '49',
         productRating: ''
     },
     {
         id: 14,
         productName: 'LED Desk Lamp',
-        productImageURL: '',
+        productImageURL: '/assets/desk_lamp.png',
         productCategory: 'Home & Office',
-        productPrice: '$29',
+        productPrice: '29',
         productRating: ''
     },
     {
         id: 15,
-        productName: 'Stainless Steel Water Bottle',
-        productImageURL: '',
+        productName: 'Water Bottle',
+        productImageURL: '/assets/stainless_steel_water_bottle.png',
         productCategory: 'Kitchen',
-        productPrice: '$15',
+        productPrice: '15',
         productRating: ''
     },
     {
         id: 16,
         productName: 'Non-Stick Cookware Set',
-        productImageURL: '',
+        productImageURL: '/assets/cookware_set.png',
         productCategory: 'Kitchen',
-        productPrice: '$89',
+        productPrice: '89',
         productRating: ''
     },
     {
         id: 17,
         productName: 'Yoga Mat',
-        productImageURL: '',
+        productImageURL: '/assets/yoga_mat.png',
         productCategory: 'Sports & Fitness',
-        productPrice: '$25',
+        productPrice: '25',
         productRating: ''
     },
     {
         id: 18,
         productName: 'Resistance Bands Set',
-        productImageURL: '',
+        productImageURL: '/assets/resistance_bands_set.png',
         productCategory: 'Sports & Fitness',
-        productPrice: '$19',
+        productPrice: '19',
         productRating: ''
     },
     {
         id: 19,
         productName: 'Artificial Plant Decor',
-        productImageURL: '',
+        productImageURL: '/assets/artificial_plants.png',
         productCategory: 'Home Decor',
-        productPrice: '$39',
+        productPrice: '39',
         productRating: ''
     },
     {
         id: 20,
         productName: 'Bluetooth Speaker',
-        productImageURL: '',
+        productImageURL: '/assets/bt_speaker.png',
         productCategory: 'Electronics',
-        productPrice: '$59',
+        productPrice: '59',
         productRating: ''
     }
 ];
